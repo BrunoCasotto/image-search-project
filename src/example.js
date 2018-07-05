3049d93a66f00849c3937d0ac7a7b96a3fc4e8ca
Bruno casotto <casottoalves@hotmail.com>
2018-07-05 00:46:55 -0300

    setup index first method

--- src/example.js
+++ src/example.js
@@ -1,3 +1,14 @@
 import Core from './'
 
 let core = new Core({delay: 500})
+
+core.call('up', 1);
+core.call('up', 2);
+core.call('up', 3);
+core.call('up', 4);
+core.call('up', 5);
+
+core.call('down', 2);
+core.call('down', 3);
+core.call('down', 4);
+core.call('down', 5);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 
-const Core = options => {
-  let {delay} = options
-  console.log(delay, 'tst')
+class Core {
+  
+  constructor({delay}) {
+    this.delay = delay
+    this.state = {
+      queueToUp: [],
+      queueToDown: []
+    }
+  }
+
+  /**
+   * function to call a elevator to up or down
+   * @param {string} action action to up or down
+   * @param {number} floor number of floot tha is calling 
+   */
+  call(action, floor) {
+    if (action === 'up') {
+      this.state.queueToUp.push(floor)
+    } else {
+      this.state.queueToDown.push(floor)
+    }
+  
+    this.updateQueue()
+  }
+
+  updateQueue() {
+    console.log(this.state);
+  }
+
 }
 
-module.exports = Core
+export default Core
